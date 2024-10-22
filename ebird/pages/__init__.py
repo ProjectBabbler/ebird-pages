04a85d10cf76cd56046a02a4cc612d197a4ae9d5
Stuart MacKay <smackay@flagstonesoftware.com>
2024-10-22 19:27:53 +0100

    project: Update package version number so it has three parts

--- ebird/pages/__init__.py
+++ ebird/pages/__init__.py
@@ -2,9 +2,8 @@
 
 """A set of functions for scraping data from eBird web pages."""
 
-__version__ = '0.1'
+__version__ = '0.1.0'
 
 # Import all the functions that make up the public API.
 # noinspection PyUnresolvedReferences
 from ebird.pages.checklists import get_checklist
-
